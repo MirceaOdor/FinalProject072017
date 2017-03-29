ec8874b3ebe7da883b6f740a726cd27fcf5cf5bf
Mircea <mircea.odor@gmail.com>
2017-03-29 14:30:39 +0200

    DB si cacaturi

---
+++ src/DBLayer/DBConnection.java
@@ -0,0 +1,21 @@
+package DBLayer;
+
+/**
+ * Created by Mircea on 29-Mar-17.
+ */
+public class DBConnection {
+
+
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:sqlserver://kraka.ucn.dk:1433;databaseName=dmaj0916_197290";
+
+    //  Database credentials
+    static final String USER = "dmaj0916_197290";
+    static final String PASS = "Password1!";
+
+
+
+
+
+
+}
