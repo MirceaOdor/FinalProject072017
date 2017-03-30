c677af58274f1190e4758f232847e2b6a52160eb
Mircea <mircea.odor@gmail.com>
2017-03-30 12:33:49 +0200

    Bag pula in baza de date

--- src/DBLayer/DBPerson.java
+++ src/DBLayer/DBPerson.java
@@ -12,33 +12,33 @@ public class DBPerson implements IFDBPer {
     private Connection con;
 
     /**
-     * Creates a new instance of DBEmployee
+     * Creates a new instance of DBPerson
      */
-    public DBEmployee() {
+    public DBPerson() {
         con = DBConnection.getInstance().getDBcon();
     }
 
     //Implements the methods from the interface
-    // get all employees
-    public ArrayList<Person> getAllEmployees(boolean seeAssociation) {
+    // get all persons
+    public ArrayList<Person> getAllPersons(boolean seeAssociation) {
         return miscWhere("", retriveAssociation);
     }
 
-    //get one employee having the ssn
-    public Employee findEmployee(String empssn, boolean retriveAssociation) {
-        String wClause = "  ssn = '" + empssn + "'";
+    //get one employee having the ID
+    public Person findPerson(String empID, boolean retriveAssociation) {
+        String wClause = "  ID = '" + empID + "'";
         return singleWhere(wClause, retriveAssociation);
     }
 
     //find one employee having the fname
-    public Employee searchEmployeeFname(String attValue, boolean retriveAssociation) {
+    public Person searchPersonFname(String attValue, boolean retriveAssociation) {
         String wClause = "fname like '%" + attValue + "%'";
         System.out.println("SearchEmployye " + wClause);
         return singleWhere(wClause, retriveAssociation);
     }
 
     //find one employee having the lname
-    public Employee searchEmployeeLname(String attValue, boolean retriveAssociation) {
+    public Person searchPersonLname(String attValue, boolean retriveAssociation) {
         String wClause = "lname = '" + attValue + "'";
         System.out.println("SearchEmployye " + wClause);
         return singleWhere(wClause, retriveAssociation);
@@ -46,17 +46,17 @@ public class DBPerson implements IFDBPer {
 
     //insert a new employee
     @Override
-    public int insertEmployee(Employee emp) throws Exception {  //call to get the next ssn number
-        int nextSSN = GetMax.getMaxId("Select max(ssn) from employee");
-        nextSSN = nextSSN + 1;
-        System.out.println("next ssn = " + nextSSN);
+    public int insertPerson(Person emp) throws Exception {  //call to get the next ID number
+        int nextID = GetMax.getMaxId("Select max(ID) from person");
+        nextID = nextID + 1;
+        System.out.println("next ID = " + nextID);
 
         int rc = -1;
-        String query = "INSERT INTO employee(fname, lname, ssn,super_ssn,dno)  VALUES('" +
+        String query = "INSERT INTO Person(fname, lname, ID,super_ID,dno)  VALUES('" +
                 emp.getFname() + "','" +
                 emp.getLname() + "','" +
-                nextSSN + "','" +
-                emp.getSupervisor().getSsn() + "',5)";
+                nextID + "','" +
+                emp.getSupervisor().getID() + "',5)";
 
 
         System.out.println("insert : " + query);
@@ -67,25 +67,29 @@ public class DBPerson implements IFDBPer {
             stmt.close();
         }//end try
         catch (SQLException ex) {
-            System.out.println("Employee ikke oprettet");
-            throw new Exception("Employee is not inserted correct");
+            System.out.println("Person ikke oprettet");
+            throw new Exception("Person is not inserted correct");
         }
         return (rc);
     }
 
     @Override
-    public int updateEmployee(Employee emp) {
-        Employee empObj = emp;
+    public int updatePerson(Person emp) {
+        Person empObj = emp;
         int rc = -1;
 
         String query = "UPDATE employee SET " +
-                "fname ='" + empObj.getFname() + "', " +
-                "lname ='" + empObj.getLname() + "', " +
-                "salary ='" + empObj.getSalary() + "', " +
-                "super_ssn ='" + empObj.getSupervisor().getSsn() + "' " +
-                " WHERE ssn = '" + empObj.getSsn() + "'";
+                "first_name ='" + empObj.getF_name() + "', " +
+                "last_name ='" + empObj.getL_name() + "', "  +
+                "cpr ='" + empObj.getCPR() + "', "  +
+                "address ='" + empObj.getAddress() + "', "  +
+                "PhoneNumber ='" + empObj.getPhNr() + "', "  +
+                "city ='" + empObj.getCity() + "', "  +
+                "Zip Code ='" + empObj.getZip() + "', "  +
+                "function ='" + empObj.getFunction() + "', "  +
+                " WHERE ID = '" + empObj.getId() + "'";
         System.out.println("Update query:" + query);
-        try { // update employee
+        try { // update person
             Statement stmt = con.createStatement();
             stmt.setQueryTimeout(5);
             rc = stmt.executeUpdate(query);
@@ -93,16 +97,16 @@ public class DBPerson implements IFDBPer {
             stmt.close();
         }//slut try
         catch (Exception ex) {
-            System.out.println("Update exception in employee db: " + ex);
+            System.out.println("Update exception in person db: " + ex);
         }
         return (rc);
     }
 
-    public int delete(String ssn) {
+    public int delete(String ID) {
         int rc = -1;
 
-        String query = "DELETE FROM employee WHERE ssn = '" +
-                ssn + "'";
+        String query = "DELETE FROM Person WHERE ID = '" +
+                ID + "'";
         System.out.println(query);
         try { // delete from employee
             Statement stmt = con.createStatement();
@@ -111,7 +115,7 @@ public class DBPerson implements IFDBPer {
             stmt.close();
         }//slut try
         catch (Exception ex) {
-            System.out.println("Delete exception in employee db: " + ex);
+            System.out.println("Delete exception in person db: " + ex);
         }
         return (rc);
     }
@@ -120,9 +124,9 @@ public class DBPerson implements IFDBPer {
     //miscWere is used whenever we want to select more than one employee
 
 
-    private ArrayList<Employee> miscWhere(String wClause, boolean retrieveAssociation) {
+    private ArrayList<Person> miscWhere(String wClause, boolean retrieveAssociation) {
         ResultSet results;
-        ArrayList<Employee> list = new ArrayList<Employee>();
+        ArrayList<Person> list = new ArrayList<Person>();
 
         String query = buildQuery(wClause);
 
@@ -133,15 +137,15 @@ public class DBPerson implements IFDBPer {
 
 
             while (results.next()) {
-                Employee empObj = new Employee();
+                Person empObj = new Person();
                 empObj = buildEmployee(results);
                 list.add(empObj);
             }//end while
             stmt.close();
             if (retrieveAssociation) {   //The supervisor and department is to be build as well
-                for (Employee empObj : list) {
-                    String superssn = empObj.getSupervisor().getSsn();
-                    Employee superEmp = singleWhere(" ssn = '" + superssn + "'", false);
+                for (Person empObj : list) {
+                    String superID = empObj.getSupervisor().getID();
+                    Person superEmp = singleWhere(" ID = '" + superID + "'", false);
                     empObj.setSupervisor(superEmp);
                     System.out.println("Supervisor is seleceted");
                     // here the department has to be selected as well
@@ -157,9 +161,9 @@ public class DBPerson implements IFDBPer {
     }
 
     //Singlewhere is used when we only select one employee
-    private Employee singleWhere(String wClause, boolean retrieveAssociation) {
+    private Person singleWhere(String wClause, boolean retrieveAssociation) {
         ResultSet results;
-        Employee empObj = new Employee();
+        Person empObj = new Person();
 
         String query = buildQuery(wClause);
         System.out.println(query);
@@ -170,11 +174,11 @@ public class DBPerson implements IFDBPer {
 
 
             if (results.next()) {
-                empObj = buildEmployee(results);
+                empObj = buildPerson(results);
                 //association is to be build
                 if (retrieveAssociation) {   //The supervisor and department is to be build as well
-                    String superssn = empObj.getSupervisor().getSsn();
-                    Employee superEmp = singleWhere(" ssn = '" + superssn + "'", false);
+                    String superID = empObj.getSupervisor().getID();
+                    Person superEmp = singleWhere(" ID = '" + superID + "'", false);
                     empObj.setSupervisor(superEmp);
                     System.out.println("Supervisor is seleceted");
                     // here the department has to be selected as well
@@ -193,7 +197,7 @@ public class DBPerson implements IFDBPer {
 
     //method to build the query
     private String buildQuery(String wClause) {
-        String query = "SELECT fname, minit,lname,ssn, address, bdate,sex, salary, super_ssn,dno  FROM employee";
+        String query = "SELECT fname, minit,lname,ID, address, bdate,sex, salary, super_ID,dno  FROM employee";
 
         if (wClause.length() > 0)
             query = query + " WHERE " + wClause;
@@ -202,27 +206,27 @@ public class DBPerson implements IFDBPer {
     }
 
     //method to build an employee object
-    private Employee buildEmployee(ResultSet results) {
-        Employee empObj = new Employee();
+    private Person buildEmployee(ResultSet results) {
+        Person empObj = new Person();
         try { // the columns from the table emplayee  are used
             empObj.setFname(results.getString("fname"));
             empObj.setMinit(results.getString("minit"));
             empObj.setLname(results.getString("lname"));
-            empObj.setSsn(results.getString("ssn"));
+            empObj.setID(results.getString("ID"));
             empObj.setBdate(results.getString("bdate"));
             empObj.setAddress(results.getString("address"));
             empObj.setSex(results.getString("sex"));
             empObj.setSalary(results.getDouble("salary"));
-            empObj.setSupervisor(new Employee(results.getString("super_ssn")));
+            empObj.setSupervisor(new Person(results.getString("super_ID")));
             empObj.setDepartment(new Department(results.getInt("dno")));
         } catch (Exception e) {
-            System.out.println("error in building the employee object");
+            System.out.println("error in building the person object");
         }
         return empObj;
     }
 
     @Override
-    public Employee searchEmployeeSsn(String ssn, boolean b) {
+    public Person searchPersonID(String ID, boolean b) {
         // TODO Auto-generated method stub
         return null;
     }
