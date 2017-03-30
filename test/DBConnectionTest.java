3ceb722ea8282ab0e8e50c38194b637f2dd9c6d0
Mircea <mircea.odor@gmail.com>
2017-03-30 11:41:46 +0200

    Bag pula in baza de date

--- test/DBConnectionTest.java
+++ test/DBConnectionTest.java
@@ -1,27 +1,48 @@
 /**
  * Created by Mircea on 30-Mar-17.
  */
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
 
-import static org.junit.Assert.*;
 public class DBConnectionTest {
 
-    DBConnection con = null;
-    static PBuy tempPBuy;
+    public static void main(String[] av) {
+        String dbURL = "jdbc:sqlserver://kraka.ucn.dk:1433;databaseName=dmaj0916_197290";
+        try {
+            // Load the jdbc-odbc bridge driver
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
 
-    /** Fixture for pay station testing. */
+            // Enable logging
+            DriverManager.setLogWriter(new PrintWriter((System.err)));
 
+            System.out.println("Getting Connection");
+            Connection conn = DriverManager.getConnection(dbURL, "dmaj0916_197290", "Password1!"); // user,
+            // passwd
 
+            // If a SQLWarning object is available, print its
+            // warning(s). There may be multiple warnings chained.
 
-    @Test
-    public void wasConnected() {
-        assertNotNull("Connected - connection cannot be null", con);
+            SQLWarning warn = conn.getWarnings();
+            while (warn != null) {
+                System.out.println("SQLState: " + warn.getSQLState());
+                System.out.println("Message:  " + warn.getMessage());
+                System.out.println("Vendor:   " + warn.getErrorCode());
+                System.out.println("");
+                warn = warn.getNextWarning();
+            }
 
-        DBConnection.closeConnection();
-        boolean wasNullified = DBConnection.instanceIsNull();
-        assertTrue("Disconnected - instance set to null", wasNullified);
+            // Do something with the connection here...
 
-        con = DBConnection.getInstance();
-        assertNotNull("Connected - connection cannot be null", con);
+            conn.close(); // All done with that DB connection
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Can't load driver " + e);
+        } catch (SQLException e) {
+            System.out.println("Database access failed " + e);
+        }
     }
 
 }
